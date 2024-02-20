1c42de7bfbe3da3f2491465052a12c3a493adf42
Agav <fensagav@gmail.com>
2024-02-20 14:08:11 +0300

    1

--- ConsoleApplication7/ConsoleApplication7.cpp
+++ ConsoleApplication7/ConsoleApplication7.cpp
@@ -1,38 +1,31 @@
 ﻿#include <iostream>
 using namespace std;
 
-const int ROWS = 3;
-const int COLS = 5;
-
-void inputArray(float arr[][COLS]) {
-    setlocale(LC_ALL, "RUS");
-    cout << "Введите элементы массива:\n";
-    for (int i = 0; i < ROWS; ++i) {
-        cout << "Строка " << i + 1 << ":\n";
-        for (int j = 0; j < COLS; ++j) {
-            cout << "Элемент " << j + 1 << ": ";
-            cin >> arr[i][j];
-        }
-    }
-}
-
-float averageOfRow(float row[], int size) {
-    float sum = 0.0;
-    for (int i = 0; i < size; ++i) {
-        sum += row[i];
-    }
-    return sum / size;
-}
-
 int main() {
-    float array[ROWS][COLS];
-    inputArray(array);
+    setlocale(LC_ALL, "RUS");
+    const int a = 3;
+    const int b = 5;
 
-    cout << "Средние арифметические элементов строк:\n";
-    for (int i = 0; i < ROWS; ++i) {
-        float avg = averageOfRow(array[i], COLS);
-        cout << "Строка " << i + 1 << ": " << avg << endl;
+    float array[a][b];
+    
+    cout << "Введите данные для элемента массива жи есть:\n";
+    for (int i = 0; i < a; ++i) {
+        cout << "Строчка " << i + 1 << ":\n";
+        for (int j = 0; j < b; ++j) {
+            cout << "Элемент " << j + 1 << ": ";
+            cin >> array[i][j];
+        }
+        cout << endl;
     }
 
-    return 0;
+    cout << "\nСредние ариф-кое элементов строк:\n";
+    for (int i = 0; i < a; ++i) {
+        float sum = 0.0;
+        for (int j = 0; j < b; ++j) {
+            sum += array[i][j];
+        }
+        float avg = sum / b;
+        cout << "Строчка " << i + 1 << ": " << avg << endl;
+    }
+
 }
